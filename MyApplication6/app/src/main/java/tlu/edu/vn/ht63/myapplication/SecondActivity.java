c18d69469de05dc56bccec74e21a41b6278deb3f
Kien <92088660+KienDuc24@users.noreply.github.com>
2024-09-12 16:29:04 +0700

    up

---
+++ MyApplication6/app/src/main/java/tlu/edu/vn/ht63/myapplication/SecondActivity.java
@@ -0,0 +1,38 @@
+package tlu.edu.vn.ht63.myapplication;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private Button btn_back2Main;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_second);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btn_back2Main = (Button) findViewById(R.id.btn_back2Main);
+        btn_back2Main.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick( View v){
+                Intent myIntent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(myIntent);
+            }
+        });
+    }
+
+}
